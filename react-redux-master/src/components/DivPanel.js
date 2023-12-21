8c8115ed2b2cda6aecd2cf10a538afb1f5051383
engin <engin2370@gmail.com>
2023-12-21 10:31:44 -0500

    add redux example

--- react-redux-master/src/components/DivPanel.js
+++ react-redux-master/src/components/DivPanel.js
@@ -1,1 +1,13 @@
+import React from "react";
+import { useSelector } from "react-redux/es/hooks/useSelector";
 
+const DivPanel =() =>{
+    let counterVal = useSelector(state => state.counter);
+    return(
+        <div>
+            The present value of counter is {counterVal}
+        </div>
+    )
+}
+
+export default DivPanel;
